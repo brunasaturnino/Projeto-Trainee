24aebcf001985d5484e36d2225223d729a4e2c87
Leviathan <ojoaosoaressantos@gmail.com>
2024-05-26 16:17:35 -0300

    Feat: Função retornar musica por id.

--- src/service/musicas/musicasService.ts
+++ src/service/musicas/musicasService.ts
@@ -16,4 +16,35 @@ class musicasService {
         
     }
 
+    async getMusicaById(id: number) {
+        try {
+            const musica: Musicas | null = await prisma.musicas.findUnique({
+                where: {
+                    id: id,
+                },
+            });
+            return musica;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateArtistById(id: number, artista: Artistas) {
+        try {
+            const artist: Artistas | null = await prisma.artistas.update({
+                data: {
+                    nome: artista.nome,
+                    foto: artista.foto,
+                    streams: artista.streams 
+                },
+                where: {
+                    id: id
+                }
+            });
+            return artist;
+        } catch (error) {
+            throw error;
+        }
+    }
+
 }
