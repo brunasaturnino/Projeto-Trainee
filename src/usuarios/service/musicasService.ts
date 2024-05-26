306e8844bcb13283b0f3f2ddef45b985cf3d59c7
brunasaturnino <brunasaturnino03@gmail.com>
2024-05-26 11:37:31 -0300

    feat: musicasService

---
+++ src/usuarios/service/musicasService.ts
@@ -0,0 +1,56 @@
+import { Musicas } from "@prisma/client";
+import prisma from "../../config/prismaClient";
+
+
+class musicasService {
+    async createMusica(musicas: Musicas) {
+        try {
+            await prisma.musicas.create({
+                data: musicas
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getMusicById(id: number) {
+        try {
+            const music: Musicas | null = await prisma.musicas.findUnique({
+                where: {
+                    id: id,
+                },
+            });
+            return music;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateMusicById(id: number, musica: Partial<Musicas>) {
+        try {
+            const updatedMusic = await prisma.musicas.update({
+                data: musica,
+                where: { id: id }
+            });
+            return updatedMusic;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async removeMusicById(id : number) {
+        try {
+            const music: Musicas | null = await prisma.musicas.delete({
+                where: {
+                    id: id
+                }           
+            });
+            return music;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default musicasService;
+
