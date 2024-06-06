093ed89aede07945aa164e1c03b738bee30c7995
Gabriel Liberato <gabriel.liberatomatias@gmail.com>
2024-06-06 10:41:46 -0300

    feat: Adiciona função generateJWT para gerar tokens JWT

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,8 +3,19 @@ import prisma from "../../config/prismaClient";
 import { PermissionError } from "../../errors/errors/PermissionError";
 import bcrypt from "bcrypt";
 import statusCodes from "../../utils/constants/statusCode";
+import { Users } from "@prisma/client";
+import { sign } from "jsonwebtoken";
 
-function genereteJWT() {}
+function genereteJWT(user: Users) {
+    const body = {
+        id: user.id,
+        email: user.email,
+        role: user.privileges,
+        name: user.name
+    }
+
+    const token = sign({user: body}, process.env.SECRET_KEY || "", {expiresIn: process.env.JWT_EXPIRATION});
+}
 
 export async function login(req: Request, res: Response, next: NextFunction) {
     try{
