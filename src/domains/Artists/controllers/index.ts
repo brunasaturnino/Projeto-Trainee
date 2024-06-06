0e597e73d8992877d5640d991371341a01be3273
brunasaturnino <brunasaturnino03@gmail.com>
2024-06-06 13:13:51 -0300

    fix: statusCodesArtists

--- src/domains/Artists/controllers/index.ts
+++ src/domains/Artists/controllers/index.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction } from "express";
 import artistsService from "../services/artistsService";
 import { verifyJWT } from "../../../middlewares/auth";
+import statusCodes from "../../../../utils/constants/statusCode";
 
 const router : Router = Router();
 const service = new artistsService();
@@ -8,7 +9,7 @@ const service = new artistsService();
 router.post('/', verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const artist = await service.createArtista(req.body);
-        res.status(201).json(artist);
+        res.status(statusCodes.CREATED).json(artist);
     } catch (error) {
         next(error);
     }
@@ -18,9 +19,9 @@ router.get('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunc
     try {
         const artist = await service.getArtistById(Number(req.params.id));
         if (artist) {
-            res.status(200).json(artist);
+            res.status(statusCodes.SUCCESS).json(artist);
         } else {
-            res.status(404).json({ error: 'Artist not found' });
+            res.status(statusCodes.NOT_FOUND).json({ error: 'Artist not found' });
         }
     } catch (error) {
         next(error);
@@ -30,7 +31,7 @@ router.get('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunc
 router.put('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const updatedArtist = await service.updateArtistById(Number(req.params.id), req.body);
-        res.status(200).json(updatedArtist);
+        res.status(statusCodes.SUCCESS).json(updatedArtist);
     } catch (error) {
         next(error);
     }
@@ -39,7 +40,7 @@ router.put('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunc
 router.delete('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const artist = await service.removeArtistById(Number(req.params.id));
-        res.status(200).json(artist);
+        res.status(statusCodes.SUCCESS).json(artist);
     } catch (error) {
         next(error);
     }
